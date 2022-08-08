3755e216f615ff4e86c0f328dc3e3474aefd104c
sonishreyas <sonishreyas10@gmail.com>
2022-08-09 02:50:48 +0530

    feat: moongose and middlewares added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import express from 'express';
-const app = express()
-const port = 3001
+import cors from 'cors';
+import  dotenv from "dotenv";
+import bodyParser from 'body-parser';
+import { initializeConnectionTodb } from './db';
 
+dotenv.config();
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+initializeConnectionTodb();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
